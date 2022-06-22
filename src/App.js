b483441fac9a70c4e8e65e6dc7286f2f00347b12
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-06-21 23:03:47 -0400

    Added footer and GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by Rosalie Monroy Wolfe and is open-sourced on
+        <a
+          href="https://github.com/SkittleFairy/react-weather-project"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
