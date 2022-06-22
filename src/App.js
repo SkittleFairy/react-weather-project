3a0709322c66eba956d68c93b794512b51711569
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-06-21 23:18:03 -0400

    Added spacing

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ export default function App() {
     <div className="App">
       React Weather App
       <footer>
-        This project was coded by Rosalie Monroy Wolfe and is open-sourced on
+        This project was coded by Rosalie Monroy Wolfe and is open-sourced&nbsp;
         <a
           href="https://github.com/SkittleFairy/react-weather-project"
           target="_blank"
         >
           GitHub
         </a>
+        .
       </footer>
     </div>
   );
