f58d7bc6a970d96ff1aee00f5f2657e2d5dc157b
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-06-23 00:06:16 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      React Weather App
-      <footer>
-        This project was coded by Rosalie Monroy Wolfe and is open-source{""}
-        <a
-          href="https://github.com/SkittleFairy/react-weather-project"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by Rosalie Monroy Wolfe and is open-source{""}
+          <a
+            href="https://github.com/SkittleFairy/react-weather-project"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
