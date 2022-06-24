61327bfd06a66f9d8f3555431628c23489f0369f
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-06-23 22:47:48 -0400

    Added HTML weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>React Weather App</h1>
+        <Weather />
         <footer>
-          This project was coded by Rosalie Monroy Wolfe and is open-source{""}
+          This project was coded by Rosalie Monroy Wolfe and is open-source{" "}
           <a
             href="https://github.com/SkittleFairy/react-weather-project"
             rel="noreferrer"
